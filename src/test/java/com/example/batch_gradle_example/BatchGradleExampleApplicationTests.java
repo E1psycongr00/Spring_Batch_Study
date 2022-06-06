62ca1ff5e2a8293d5ce00f1615141eda201a76cf
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-07 07:05:50 +0900

    feat: init setting

---
+++ src/test/java/com/example/batch_gradle_example/BatchGradleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batch_gradle_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchGradleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
