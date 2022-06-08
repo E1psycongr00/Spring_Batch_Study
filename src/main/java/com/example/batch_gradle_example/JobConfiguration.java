88b9f9e7de9a7a0265d5ac8301f9237028df70d1
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-06-08 22:32:56 +0900

    feat: Step Configuration 구성

--- src/main/java/com/example/batch_gradle_example/JobConfiguration.java
+++ src/main/java/com/example/batch_gradle_example/JobConfiguration.java
@@ -12,9 +12,12 @@ import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.*;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,11 +27,11 @@ public class JobConfiguration {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job batchJob1() {
-        return this.jobBuilderFactory.get("batchJob1")
-                .incrementer(new RunIdIncrementer())
+    public Job batchJob() {
+        return this.jobBuilderFactory.get("batchJob")
                 .start(step1())
                 .next(step2())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
     
@@ -49,21 +52,26 @@ public class JobConfiguration {
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step2 has executed");
-                    return RepeatStatus.FINISHED;
+                .<String, String>chunk(3)
+                .reader(new ItemReader<String>() {
+                    @Override
+                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                        return null;
+                    }
+                })
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        return null;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> items) throws Exception {
+        
+                    }
                 })
                 .build();
-    }
-    
-    @Bean
-    public Flow flow() {
-        FlowBuilder<Flow> flowFlowBuilder = new FlowBuilder<>("flow");
-        flowFlowBuilder.start(step3())
-                .next(step4())
-                .end();
-        
-        return flowFlowBuilder.build();
     }
     
     @Bean
@@ -79,18 +87,41 @@ public class JobConfiguration {
     @Bean
     public Step step3() {
         return stepBuilderFactory.get("step3")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("executed3");
-                    return RepeatStatus.FINISHED;
-                }).build();
+                .partitioner(step1())
+                .gridSize(2)
+                .build();
     }
     
     @Bean
     public Step step4() {
         return stepBuilderFactory.get("step4")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("executed4");
-                    return RepeatStatus.FINISHED;
-                }).build();
+                .job(job())
+                .build();
+    }
+    
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .flow(flow())
+                .build();
+    }
+    
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .build();
+    }
+    
+    @Bean
+    public Flow flow() {
+        FlowBuilder<Flow> flowFlowBuilder = new FlowBuilder<>("flow");
+        flowFlowBuilder.start(step1())
+                .next(step2())
+                .end();
+        
+        return flowFlowBuilder.build();
     }
 }
